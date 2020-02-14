8a4ba58f42106ccf95aca28455e9b69c17c0674e
Matteo Mortelliti <mortellitimatteo@gmail.com>
2020-02-14 16:53:47 -0500

    Major updates to typography, navigation etc

--- imageZoom.js
+++ imageZoom.js
@@ -1,71 +1,82 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img1 = document.getElementById('myImg');
+// var img1 = document.getElementById('myImg');
 
-
-const glideArrows = document.querySelectorAll('.glide__arrow');
-
+const glideArrows = document.querySelectorAll(".glide__arrow");
 
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 var modalImgs = document.querySelectorAll(".modalImage");
 
-//loop attempt 
+const firstSibling = document.getElementById("firstImg");
+const lastSibling = document.getElementById("lastImg");
+
+//solved loop issue by getting the sibling of the active element.
+
+var activeElement = document.getElementsByClassName("glide__slide--active");
+
+//loop attempt on zoom and actual zoom
 window.onload = function loop() {
   for (i = 0; i < modalImgs.length; i++) {
-    modalImgs[i].addEventListener("click", function () {
+    modalImgs[i].addEventListener("click", function() {
       modal.style.display = "block";
-      modalImg.src = this.src;
-      glideArrows[1].style.visibility = "hidden";
-      glideArrows[0].style.visibility = "hidden";
+      var clickedImg = this;
+      modalImg.src = clickedImg.src;
       modalImg.alt = this.alt;
-    });
+      glideArrows[0].classList.add("zoomed");
+      glideArrows[1].classList.add("zoomed");
+      glideArrows[0].style.color = "white";
+      glideArrows[1].style.color = "white";
+      glideArrows[0].style.margin = "0px -10px";
+      glideArrows[1].style.margin = "0px -10px";
 
+      //loop attempt is here
+
+      glideArrows[1].addEventListener("click", function() {
+        modalImg.src = activeElement[0].nextElementSibling.childNodes[1].src;
+      });
+
+      glideArrows[0].addEventListener("click", function() {
+        modalImg.src =
+          activeElement[0].previousElementSibling.childNodes[1].src;
+      });
+    });
   }
 };
 
-// original code
-// img1.onclick = function () {
-//   console.log(this);
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   modalImg.alt = this.alt;
-//   captionText.innerHTML = this.alt;
-// }
-
-
-//removes the arrows on click (added to loop function)
-
-// img1.addEventListener("click", function () {
-//   glideArrows[1].style.visibility = "hidden";
-//   glideArrows[0].style.visibility = "hidden";
-// })
-
-
-// When the user clicks on <span> (x), close the modal - click anywwhere and it's gone 
-modal.onclick = function () {
+// When the user clicks on <span> (x), close the modal - click anywwhere and it's gone
+modal.onclick = function() {
   img01.className += " out";
-  setTimeout(function () {
+
+  setTimeout(function() {
     modal.style.display = "none";
     img01.className = "modal-content";
-  }, 400);
-  glideArrows[1].style.visibility = "visible";
-  glideArrows[0].style.visibility = "visible";
-}
 
+    glideArrows[0].classList.remove("zoomed");
+    glideArrows[1].classList.remove("zoomed");
+    glideArrows[0].style.color = "black";
+    glideArrows[1].style.color = "black";
+    glideArrows[0].style.margin = "0px -20px";
+    glideArrows[1].style.margin = "0px -20px";
+  }, 400);
+};
 
 //closes modal on keydown
 
 window.addEventListener("keydown", event => {
   if (modal.style.display === "block") {
     img01.className += " out";
-    setTimeout(function () {
+    setTimeout(function() {
       modal.style.display = "none";
       img01.className = "modal-content";
+      glideArrows[0].classList.remove("zoomed");
+      glideArrows[1].classList.remove("zoomed");
+      glideArrows[0].style.color = "black";
+      glideArrows[1].style.color = "black";
+      glideArrows[0].style.margin = "0px -20px";
+      glideArrows[1].style.margin = "0px -20px";
     }, 400);
-    glideArrows[1].style.visibility = "visible";
-    glideArrows[0].style.visibility = "visible";
   } else false;
-})
+});
--- script.js
+++ script.js
@@ -1,45 +1,176 @@
-// let hammer = document.querySelector(".hammer");
-
-// let menu = document.querySelector(".menu");
-// let menuLinks = menu.querySelectorAll("a");
-
-// menu.addEventListener("mouseover", function() {
-//   hammer.style.color = "blue";
-// });
-
 let hamburger = document.querySelector("#toggle");
 let menu = document.querySelector(".menu");
 
-
-hamburger.addEventListener('click', function () {
-  (menu.style.transition = "all 1s ease");
+hamburger.addEventListener("click", function () {
+  menu.style.transition = "all .5s ease";
 });
 
 window.onresize = function () {
   if (window.innerWidth > 718) {
-    menu.style.transition = "none"
+    menu.style.transition = "none";
   }
-}
+};
 
 //glider - options https://glidejs.com/docs/options/
 
 const config = {
-  type: 'carousel',
-  perView: 3,
-  focusAt: 'center',
+  type: "carousel",
+  perView: 4,
+  focusAt: "center",
   gap: 8,
   keyboard: true,
   animationDuration: 500,
-  animationTimingFunc: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)',
+  animationTimingFunc: "cubic-bezier(0.25, 0.46, 0.45, 0.94)",
   breakpoints: {
-    1140: {
+    1500: {
+      perView: 3
+    },
+    1200: {
       perView: 2,
       focusAt: 0
     },
-    750: {
+    810: {
       perView: 1,
       focusAt: "center"
+    },
+    400: {
+      perView: 1,
+      focusAt: 0,
+      gap: 15
     }
   }
+};
+new Glide(".glide", config).mount();
+
+//navbar changes on scroll
+
+const navbar = document.querySelector(".nav");
+const topHeader = document.querySelector(".topHeader");
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 120 && window.innerWidth > 768) {
+    navbar.classList.add("nav-scrolled");
+  } else {
+    navbar.classList.remove("nav-scrolled");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  if (window.pageYoffset > 120) {
+    topHeader.classList.add("displayNone");
+  } else {
+    topHeader.classList.add("displayOn");
+  }
+});
+
+
+//REMOVE MENU DROPBOWN ON SCROLL (need to ffix!!!) try adding more if statements (if the toggle is rotated) and when the scroll happens the toggle should be "checked"
+
+
+
+let menuHover = function () {
+  let firstPosY = event.deltaY;
+  let newPosY = firstPosY + event.deltaY;
+  if (window.innerWidth <= 768 && firstPosY < newPosY) {
+    scrollingDown = true;
+
+    navbar.style.transitionDuration = ".2s";
+    navbar.style.opacity = "0";
+    navbar.style.transform = "translate(0, -80px)";
+
+  } else if (firstPosY > newPosY) {
+    navbar.style.opacity = "1";
+    navbar.style.transform = "translate(0, 0px)";
+
+  }
+
+
+
+
 }
-new Glide('.glide', config).mount()
+
+
+
+
+
+
+
+
+
+
+
+//appear transition effect
+
+let aboutHeading = document.querySelector(".aboutHeading");
+let aboutText = document.querySelector(".aboutText");
+let servicesTitle1 = document.querySelector("#box1 h3");
+let servicesTitle2 = document.querySelector("#box2 h3");
+let servicesTitle3 = document.querySelector("#box3 h3");
+let servicesText1 = document.querySelector("#box1 p");
+let servicesText2 = document.querySelector("#box2 p");
+let servicesText3 = document.querySelector("#box3 p");
+let helpTitle = document.querySelector(".helpTitle");
+let helpText = document.querySelector(".helpText");
+let helpButton = document.querySelector(".helpButton");
+let portfolioHeading = document.querySelector(".portfolioHeading");
+let contactText = document.querySelector(".contactText");
+
+function scrollAppear(variable) {
+  var introPosition = variable.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight;
+
+  if (introPosition < screenPosition / 1.2) {
+    variable.classList.add("text-appear");
+  }
+}
+
+document.addEventListener("scroll", function () {
+  scrollAppear(aboutHeading);
+  scrollAppear(aboutText);
+  scrollAppear(servicesTitle1);
+  scrollAppear(servicesTitle2);
+  scrollAppear(servicesTitle3);
+  scrollAppear(servicesText1);
+  scrollAppear(servicesText2);
+  scrollAppear(servicesText3);
+  scrollAppear(helpTitle);
+  scrollAppear(helpText);
+  scrollAppear(helpButton);
+  scrollAppear(portfolioHeading);
+  // scrollAppear(contactText);
+});
+
+// get label
+
+var label = document.getElementById("label");
+
+var toggle = document.getElementById("toggle");
+
+toggle.addEventListener("change", function () {
+  if (this.checked) {
+    label.innerHTML = "&#65291;";
+    label.style.fontSize = "2.6rem";
+    label.style.color = "#E81A16";
+  } else {
+    label.innerHTML = "&#9776;";
+    label.style.fontSize = "1.8em";
+    label.style.color = "#051821";
+  }
+});
+
+
+
+
+
+// if the menu a get clicked when the toggle is checked, uncheck it. or if user scrolls when checked, uncheck it.
+
+menu.children[0].addEventListener("click", function () {
+  toggle.checked = false;
+  label.innerHTML = "&#9776;";
+  label.style.fontSize = "1.8em";
+  label.style.color = "#051821";
+});
+
+//smooth scroll js
+
+window.addEventListener("mousewheel", menuHover);
